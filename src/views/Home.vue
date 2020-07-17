2cbc026ae8cc77a56e2b6c1a287979515460099c
Eric <13522679763@163.com>
2020-07-17 17:05:23 +0800

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+       首页
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+<script type="text/ecmascript-6">
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
+
+<style   lang="scss"  scoped>
+
+</style>
